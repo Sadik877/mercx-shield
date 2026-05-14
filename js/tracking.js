c8f408dd7f3f34dbb6cbeb5cac0aa6b98e64c78c
Sadik877 <153235973+Sadik877@users.noreply.github.com>
2026-05-14 03:00:48 +0100

    Update tracking.js

--- js/tracking.js
+++ js/tracking.js
@@ -1,1 +1,10 @@
+function initMap() {
+  const driverLocation = { lat: 12.0022, lng: 8.5919 }; // Kano coords
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 14,
+    center: driverLocation,
+  });
+  new google.maps.Marker({ position: driverLocation, map: map });
+}
+window.onload = initMap;
 
