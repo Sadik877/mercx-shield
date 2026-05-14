890527029ba31dce4c52fa68fa558ecae1ddaab6
Sadik877 <153235973+Sadik877@users.noreply.github.com>
2026-05-14 02:58:55 +0100

    Update driver.js

--- js/driver.js
+++ js/driver.js
@@ -1,1 +1,13 @@
+function acceptOrder() {
+  let order = JSON.parse(localStorage.getItem('order'));
+  order.status = 'accepted';
+  localStorage.setItem('order', JSON.stringify(order));
+  alert('Order accepted!');
+}
+
+function rejectOrder() {
+  let order = JSON.parse(localStorage.getItem('order'));
+  order.status = 'pending';
+  alert('Order rejected!');
+}
 
