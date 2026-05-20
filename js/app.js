4faebd143de884471e4e0c160e602a0e0bb83c2b
Sadik877 <153235973+Sadik877@users.noreply.github.com>
2026-05-20 16:40:27 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,34 @@
+const loader =
+document.getElementById('loader');
+
+window.addEventListener('load', () => {
+
+setTimeout(() => {
+
+loader.style.opacity = '0';
+
+loader.style.visibility = 'hidden';
+
+}, 1500);
+
+});
+
+/* NAV SHADOW */
+
 window.addEventListener('scroll', () => {
 
-  const nav = document.querySelector('nav');
+const nav =
+document.querySelector('nav');
 
-  if(window.scrollY > 50){
-    nav.style.boxShadow = '0 10px 40px rgba(0,0,0,0.25)';
-  } else {
-    nav.style.boxShadow = 'none';
-  }
+if(window.scrollY > 50){
+
+nav.style.boxShadow =
+'0 10px 40px rgba(0,0,0,.35)';
+
+}else{
+
+nav.style.boxShadow = 'none';
+
+}
 
 });
